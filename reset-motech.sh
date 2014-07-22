340e9e3d1db5f2d165bed4b394c0a76f15e1be3d
Jakub Kondrat <jkondrat@soldevelo.com>
2014-07-22 17:31:52 +0200

    Added reset-motech.sh

---
+++ reset-motech.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+MVN="mvn clean install -U -Dmaven.test.skip=true"
+MOTECH_CONFIG=$HOME/.motech
+#CATALINA_HOME=$HOME/apache-tomcat-7.0.22
+
+MOTECH_TRUNK=$HOME/motech/motech
+MOTECH_BRANCH=origin/0.24.X
+MOTECH_BRANCH_OLD=tags/motech-0.22
+
+MOTECH_COMMUNICATIONS=$HOME/motech/motech-communications
+MOTECH_MODULES=$HOME/motech/modules
+MODULES_BRANCH=dev/mtraining2mds
+
+IMPL=$HOME/motech/whp-mTraining
+MOTECH_SCRIPTS=$HOME/motech/motech-scripts
+
+function stopTomcat {
+    $CATALINA_HOME/bin/catalina.sh jpda stop
+    sleep 2
+	kill -9 $(ps aux | grep '[t]omcat' | awk '{print $2}')
+}
+
+function startTomcat {
+	echo "" > $CATALINA_HOME/logs/catalina.out
+    $CATALINA_HOME/bin/catalina.sh jpda start
+	tail -f $CATALINA_HOME/logs/catalina.out
+}
+
+function restartTomcat {
+    stopTomcat
+    startTomcat
+}
+
+function createQuartzDb {
+    mysql -u root -ppassword -e 'drop database if exists motechquartz; create database motechquartz'
+    checkoutPlatform $1
+	if [ "$1" == $MOTECH_BRANCH_OLD ]; then
+		mysql -u root -ppassword motechquartz < $MOTECH_TRUNK/modules/scheduler/scheduler/sql/create_db_schema_quartz_v2.1.sql
+	else 
+		mysql -u root -ppassword motechquartz < $MOTECH_TRUNK/modules/scheduler/sql/create_db_schema_quartz_v2.1.sql
+	fi
+}
+
+function removeCouchDatabase {
+    $MOTECH_SCRIPTS/db-remove-motech
+}
+
+function checkoutPlatform {
+	cd $MOTECH_TRUNK
+	git reset --hard
+    git checkout $1
+	if [ "$1" == $MOTECH_BRANCH ]; then
+		git cherry-pick f1f47ea768bafc9b0855d9c9011d3f8cd33316db
+	fi
+}
+
+function modifyPlatformPom {
+	cd $MOTECH_TRUNK
+	awk 'NR==164{$0="				<dependencies>\n\
+				<!-- override the default dependency -->\n\
+				<dependency>\n\
+					<groupId>org.datanucleus</groupId>\n\
+					<artifactId>datanucleus-core</artifactId>\n\
+					<version>3.2.11</version>\n\
+				</dependency>\n\
+			</dependencies>\n"$0}1' modules/mds/pom.xml > res.txt
+	mv res.txt modules/mds/pom.xml
+}
+
+function rebuildPlatform {
+    rm -rf $MOTECH_CONFIG
+	checkoutPlatform $1
+	if [ "$1" == $MOTECH_BRANCH_OLD ]; then
+		modifyPlatformPom
+	fi
+	cd $MOTECH_TRUNK
+	$MVN
+}
+
+function deployPlatform {
+    rm -rf $CATALINA_HOME/webapps/motech-platform-server.war
+    rm -rf $CATALINA_HOME/webapps/motech-platform-server
+    cp $MOTECH_TRUNK/platform/server/target/motech-platform-server.war $CATALINA_HOME/webapps
+}
+
+function rebuildModules {
+	mysql -u root -ppassword -e 'drop database if exists motech_data_services; create database motech_data_services'
+	cd $MOTECH_MODULES/mtraining/
+	git checkout $MODULES_BRANCH
+	$MVN
+}
+
+function rebuildCommunications {
+	cd $MOTECH_COMMUNICATIONS
+	$MVN
+}
+
+function modifyImplPom {
+	cd $IMPL
+	awk 'NR==175{$0="            <version>3.2.10</version>"}1' mtraining/pom.xml | \
+	awk 'NR==164{$0="        <dependency>\n\
+            <groupId>javax.time</groupId>\n\
+            <artifactId>org.motechproject.javax.time</artifactId>\n\
+            <version>0.6.3-${external.dependency.release.tag}</version>\n\
+        </dependency>\n"$0}1' > res.txt
+	mv res.txt mtraining/pom.xml
+}
+
+function rebuildImpl {
+	cd $IMPL
+	if [ "$1" == $MOTECH_BRANCH_OLD ]; then
+		git reset --hard 57cc9ce87527f0996d88aa589941864689da4378
+		modifyImplPom
+	elif [ "$1" == $MOTECH_BRANCH ]; then
+		git reset --hard origin/master
+	fi
+	$MVN
+}
+
+cd $HOME
+if [ "$1" == "impl" ]; then
+    stopTomcat
+    rebuildImpl
+    startTomcat
+elif [ "$1" == "restart" ]; then
+    restartTomcat
+elif [ "$1" == "stop" ]; then
+    stopTomcat
+elif [ "$1" == "old" ]; then	
+	# TODO old version:
+	# Create database named "whp" in Postgres
+    stopTomcat
+    createQuartzDb $MOTECH_BRANCH_OLD
+    removeCouchDatabase
+    rebuildPlatform $MOTECH_BRANCH_OLD
+    rebuildCommunications
+    rebuildImpl $MOTECH_BRANCH_OLD
+    deployPlatform
+    startTomcat
+else
+    stopTomcat
+    createQuartzDb $MOTECH_BRANCH
+    removeCouchDatabase
+    rebuildPlatform $MOTECH_BRANCH
+    rebuildModules
+    rebuildImpl $MOTECH_BRANCH
+    deployPlatform
+    startTomcat
+fi
